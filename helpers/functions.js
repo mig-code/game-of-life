dc99faf0dc5e65d97bd3a5b98de35251e31fb41c
Miguel <99726342+mig-code@users.noreply.github.com>
2022-11-14 18:06:13 +0100

    Update create Board with nulls in borders

--- helpers/functions.js
+++ helpers/functions.js
@@ -1,9 +1,15 @@
 export function createBoard(columns, rows) {
     const board = [];
+    columns = columns + 2;
+    rows = rows + 2;
     for (let i = 0; i < rows; i++) {
         board.push([]);
         for (let j = 0; j < columns; j++) {
-            board[i].push(1);
+            if (i === 0 || i === rows - 1 || j === 0 || j === columns - 1) {
+                board[i].push(null);
+            } else {
+                board[i].push(1);
+            }
         }
     }
     return board;
--- helpers/functions.test.js
+++ helpers/functions.test.js
@@ -1,14 +1,25 @@
 import { createBoard } from './functions.js';
 
 describe('Given createBoard function', () => {
-    test('Return a board with all 1', () => {
+    test('Return a board with all 1 and nulls in borders test [0][0]', () => {
+        const board = createBoard(6, 6);
+
+        // Arrange
+        const expected = null;
+
+        // Act
+        const result = board[0][0];
+        // Assert
+        expect(result).toEqual(expected);
+    });
+    test('Return a board with all 1 and nulls in borders test [1][1]', () => {
         const board = createBoard(6, 6);
 
         // Arrange
         const expected = 1;
 
         // Act
-        const result = board[0][0];
+        const result = board[1][1];
         // Assert
         expect(result).toEqual(expected);
     });
