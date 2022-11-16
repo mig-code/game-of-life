17e787e49de32cd88f179a5df572f169dbb7b281
Miguel <99726342+mig-code@users.noreply.github.com>
2022-11-16 19:09:37 +0100

    Add DomFunctions.test

---
+++ helpers/domFuntcions.test.js
@@ -0,0 +1,14 @@
+import { renderBoard } from './domFunctions.js';
+
+describe('Given renderBoard function', () => {
+    const columns = 6;
+    const rows = 6;
+    test('Return a error because testing in the wrong enviroment', () => {
+        
+        const expected = 'document is not defined';
+       
+        expect(() => {
+           renderBoard(columns, rows);
+        }).toThrow(expected);
+    });
+});
