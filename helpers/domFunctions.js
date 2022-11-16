8d2ac9eea4792a77db0f675e263a4aa8fd01108a
Miguel <99726342+mig-code@users.noreply.github.com>
2022-11-16 13:15:16 +0100

    First working version

--- helpers/domFunctions.js
+++ helpers/domFunctions.js
@@ -7,12 +7,15 @@ export function renderBoard(board) {
         parentdiv.appendChild(row);
         for (let j = 1; j < board[i].length - 1; j++) {
             const childdiv = document.createElement('div');
+            childdiv.addEventListener('click', () => {
+                board[i][j] === 0 ? (board[i][j] = 1) : (board[i][j] = 0);
+                renderBoard(board);
+            });
             childdiv.className = 'cell';
             if (board[i][j] === 1) {
-                childdiv.innerText = '🦠';
                 childdiv.classList.add('cell--alive');
             }
-            if(board[i][j] === 0) {
+            if (board[i][j] === 0) {
                 childdiv.classList.add('cell--dead');
             }
 
--- index.js
+++ index.js
@@ -1,17 +1,83 @@
 import { createBoard, getNextBoard } from './helpers/functions.js';
 import { renderBoard } from './helpers/domFunctions.js';
+import {
+    emptyBoard,
+    smileBoard,
+    isdiBoard,
+    figureBoard,
+} from './helpers/boards.js';
 
 // Config section
-const columns = 40;
-const rows = 40;
-const maxGenerations = 30;
+const columns = 28;
+const rows = 28;
 const intervalTime = 1000;
+let startInterval = null;
+let board = smileBoard;
+
+// DOM elements
+const btnStart = document.querySelector('.btn--start');
+btnStart.addEventListener('click', () => {
+    startInterval = setInterval(startSimulation, intervalTime);
+    btnStart.disabled = true;
+});
+const btnStop = document.querySelector('.btn--stop');
+btnStop.addEventListener('click', () => {
+    clearInterval(startInterval);
+    btnStart.disabled = false;
+});
+const btnReset = document.querySelector('.btn--reset');
+btnReset.addEventListener('click', () => {
+    clearInterval(startInterval);
+    btnStart.disabled = false;
+    board = smileBoard;
+    renderBoard(board);
+    console.log('reset');
+});
+const btnSmile = document.querySelector('.btn--smile');
+btnSmile.addEventListener('click', () => {
+    board = smileBoard;
+    renderBoard(board);
+});
+const btnIsdi = document.querySelector('.btn--isdi');
+btnIsdi.addEventListener('click', () => {
+    board = isdiBoard;
+    renderBoard(board);
+});
+
+const btnFigure = document.querySelector('.btn--figure');
+btnFigure.addEventListener('click', () => {
+    board = figureBoard;
+    renderBoard(board);
+});
+const btnRandom = document.querySelector('.btn--random');
+btnRandom.addEventListener('click', () => {
+    board = createBoard(columns, rows);
+    renderBoard(board);
+});
+const btnDraw = document.querySelector('.btn--draw');
+btnDraw.addEventListener('click', () => {
+    clearInterval(startInterval);
+    btnStart.disabled = false;
+    board = emptyBoard;
+    renderBoard(board);
+});
+
+// DOM Actions
 
 // Init section
-let board = createBoard(columns, rows);
-let intervalCounter = 0;
+
+// On load
+window.onload = () => {
+    renderBoard(board);
+};
+
+function startSimulation() {
+    renderBoard(board);
+    board = getNextBoard(board);
+}
 
 // Render board in console
+
 // Need to import getBoardToRender
 
 // const runGame = setInterval(() => {
@@ -23,13 +89,3 @@ let intervalCounter = 0;
 //     }
 //     intervalCounter += 1;
 // }, intervalTime);
-
-const runGame = setInterval(() => {
-    renderBoard(board);
-    board = getNextBoard(board);
-
-    if (intervalCounter === maxGenerations) {
-        clearInterval(runGame);
-    }
-    intervalCounter += 1;
-}, intervalTime);
